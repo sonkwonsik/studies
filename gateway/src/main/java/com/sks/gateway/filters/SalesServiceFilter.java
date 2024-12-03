5155fd7a0f59e715ba3e8da1952c4d2c485a608a
sonkwonsik <gweonsik@gmaile.com>
2024-12-03 14:53:26 +0900

    필터추가

--- gateway/src/main/java/com/sks/gateway/filters/SalesServiceFilter.java
+++ gateway/src/main/java/com/sks/gateway/filters/SalesServiceFilter.java
@@ -12,43 +12,56 @@ import org.springframework.util.StopWatch;
 
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-	private static final Logger logger = LoggerFactory.getLogger(GlobalFilter.class);
+public class SalesServiceFilter extends AbstractGatewayFilterFactory<SalesServiceFilter.Config> {
+	private static final Logger logger = LoggerFactory.getLogger(SalesServiceFilter.class);
 	private StopWatch stopWatch;
 	
-	public GlobalFilter() {
+	public SalesServiceFilter() {
 		super(Config.class);
-		stopWatch = new StopWatch("Gateway");
+		stopWatch = new StopWatch("Sales Service Gateway");
 	}
 
 	@Override
 	public GatewayFilter apply(Config config) {
+		config.setEnabled(true);
 		// 람다 표현식을 사용하지 않고 구현
+		stopWatch.start();
 		return new GatewayFilter() {
 			@Override
 			public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 				ServerHttpRequest request = exchange.getRequest();
 				ServerHttpResponse response = exchange.getResponse();
-//				stopWatch.start();
-				logger.info("[글로벌필터] Request요청>>>>> IP : {}, URI : {}",request.getRemoteAddress().getAddress(), request.getURI());
+				logger.info("[세일즈서비스필터] Request요청>>>>> IP : {}, URI : {}, 경로 : {} ",
+						request.getRemoteAddress().getAddress(), 
+						request.getURI(), 
+						request.getMethod().toString() + request.getPath().toString());
+				for (Entry<String, List<String>> header : request.getHeaders().entrySet()) {
+				    logger.info("	[세일즈서비스필터]{} : {}:{}", this.getClass().getName(), header.getKey(), header.getValue());
+				}
 				
-				if (config.isEnabled() && exchange.getRequest().getHeaders().containsKey("X-Block")) {
+				
+//				if (config.isEnabled() && exchange.getRequest().getHeaders().containsKey("X-Block")) {
 					exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-					stopWatch.stop();
-					logger.info("[글로벌필터] Response 응답 >>>>> IP : {}, URI : {}, 응답코드 : {}, 처리시간 : {} ",
+					logger.info("[세일즈서비스필터] Response 응답 >>>>> IP : {}, URI : {}, 응답코드 : {}, 처리시간 : {} ",
 							request.getRemoteAddress().getAddress(), 
 							request.getURI(),
 							response.getStatusCode(),
 							stopWatch.getTotalTime(TimeUnit.MILLISECONDS)
 							);
-					return exchange.getResponse().setComplete();
-				}
+					for (Entry<String, List<String>> header : response.getHeaders().entrySet()) {
+					    logger.info("	[세일즈서비스필터]{} : {}:{}", this.getClass().getName(), header.getKey(), header.getValue());
+					}
+//					return exchange.getResponse().setComplete();
+//				}
+				stopWatch.stop();
 				// 다음 필터로 요청 전달
 				return chain.filter(exchange);
 			}
